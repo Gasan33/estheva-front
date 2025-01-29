ed10c5d38767abc7b31ecc8ea91169bea4a70d32
ALFAFA <gasan1288@gmail.com>
2025-01-29 16:25:33 +0400

    Update

--- app/(root)/blogs/[id]/page.tsx
+++ app/(root)/blogs/[id]/page.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { blogs } from '@/constants';
-import type { Metadata } from 'next';
+import type { Metadata, ResolvingMetadata } from 'next';
 import BlogDetails from '@/components/common/BlogDetails';
 
 type Props = {
-    params: { id: string };
-};
+    params: Promise<{ id: string }>
+    // searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-    const blog = blogs.find((item) => item.id === Number(params.id));
+export async function generateMetadata(
+    { params }: Props,
+    parent: ResolvingMetadata
+): Promise<Metadata> {
+    const id = (await params).id;
+
+    const blog = blogs.find((item) => item.id.toString() === id);
 
     return {
         title: blog ? blog.title : 'Blog Details',
@@ -19,8 +25,8 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
     };
 }
 
-const BlogPage = ({ params }: Props) => {
-    return <BlogDetails id={params.id} />;
+const BlogPage = async ({ params }: Props) => {
+    return <BlogDetails id={(await params).id} />;
 };
 
 export default BlogPage;
