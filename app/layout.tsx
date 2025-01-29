dc2bf66d9ae9dab9998558a12eea91ee5bf75453
ALFAFA <gasan1288@gmail.com>
2025-01-29 16:28:41 +0400

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ const touche = localFont({
     { path: '/fonts/Touche-Light.otf', weight: '300', style: 'normal' },
     { path: '/fonts/Touche-Regular.otf', weight: '400', style: 'normal' },
     { path: '/fonts/Touche-Medium.otf', weight: '500', style: 'normal' },
-    { path: '/fonts/Touche-SemiBold.otf', weight: '600', style: 'normal' },
+    { path: '/fonts/Touche-Semibold.otf', weight: '600', style: 'normal' },
     { path: '/fonts/Touche-Bold.otf', weight: '700', style: 'normal' }
   ]
 })
