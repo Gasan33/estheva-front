4b1fa9bab6932412559d67ba44919d3bef2e380c
ALFAFA <gasan1288@gmail.com>
2025-04-10 20:07:51 +0400

    `Removed unused import and function in sign-in and sign-up pages`

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,33 +1,9 @@
 "use client"
 
 import AuthForm from '@/components/forms/AuthForm'
-// import { signInWithCredentials } from "@/lib/actions/auth"
+import { signInWithCredentials } from "@/lib/actions/auth"
 import { signInSchema } from '@/lib/validations'
-import { signIn } from 'next-auth/react';
 import React from 'react'
-
-
-export const signInWithCredentials = async (params: Pick<AuthCredentials, "email" | "password">) => {
-    const { email, password } = params;
-
-    try {
-        const result = await signIn("credentials", {
-            email,
-            password,
-            redirect: false,
-            redirectTo: '/',
-        });
-
-        if (result?.error) {
-            return { success: false, error: result.error };
-        }
-        return { success: true };
-    } catch (error) {
-        console.error("Signin error:", error);
-        return { success: false, error: "Signin error" };
-    }
-};
-
 
 const page = () => {
     return (<AuthForm
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,42 +1,8 @@
 "use client"
 import AuthForm from '@/components/forms/AuthForm'
-// import { signup } from '@/lib/actions/auth'
+import { signup } from '@/lib/actions/auth'
 import { signUpSchema } from '@/lib/validations'
 import React from 'react'
-import { signInWithCredentials } from '../sign-in/page'
-import config from '@/lib/config'
-
-export const signup = async (params: AuthCredentials) => {
-    const { firstName, lastName, email, password, phoneNumber, confirmPassword } = params;
-
-    try {
-        const response = await fetch(`${config.env.apiEndpoint}/register`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                first_name: firstName,
-                last_name: lastName,
-                email: email,
-                phone_number: phoneNumber,
-                password: password,
-            }),
-        });
-        console.log(response)
-
-        if (!response.ok) {
-            const errorData = await response.json();
-            return { success: false, error: errorData.message || "Signup failed" };
-        }
-
-        // Automatically sign in after successful signup
-        return await signInWithCredentials({ email, password });
-    } catch (error) {
-        console.error("Signup error:", error);
-        return { success: false, error: "Signup error" };
-    }
-};
 
 const page = () => {
     return (<AuthForm
