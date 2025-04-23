e849f90f60fc26d0262b4530e6fdff0b80015150
ALFAFA <gasan1288@gmail.com>
2025-04-23 18:49:27 +0400

    `Refactor sign-in page to use signInWithCredentials action`

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,30 +1,9 @@
 "use client";
-
 import AuthForm from "@/components/forms/AuthForm";
-import { signIn } from "next-auth/react";
 import { signInSchema } from "@/lib/validations";
-import { useRouter } from "next/navigation";
-import { toast } from "@/hooks/use-toast";
+import { signInWithCredentials } from "@/lib/actions/auth";
 
 const SignInPage = () => {
-    const router = useRouter();
-
-    const handleClientSignIn = async (values: { email: string; password: string }) => {
-        const res = await signIn("credentials", {
-            ...values,
-            redirect: false,
-        });
-
-        if (res?.ok) {
-            toast({ title: "Signed in successfully" });
-            router.push("/"); // or any redirect you want
-            return { success: true };
-        } else {
-            toast({ title: "Login failed", description: res?.error || "Invalid credentials" });
-            return { success: false, error: "Signin error" };
-        }
-    };
-
     return (
         <AuthForm
             type="SIGN_IN"
@@ -33,7 +12,7 @@ const SignInPage = () => {
                 email: "",
                 password: "",
             }}
-            onSubmit={handleClientSignIn}
+            onSubmit={signInWithCredentials}
         />
     );
 };
